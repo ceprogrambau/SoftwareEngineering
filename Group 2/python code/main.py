d5e9346554d5deaef242dbf83ab8dbae9752ea8e
Mohamad Al Ghoush <mohamadghoush545@gmail.com>
2025-04-03 20:54:43 +0300

    requements docs still in progress

--- Group 2/python code/main.py
+++ Group 2/python code/main.py
@@ -5,7 +5,6 @@ dataframes_dict = fetch_all_tables_data_to_df()
 if dataframes_dict is None:
     print("Failed to fetch data from the database.")
 
-    # Print out some information about the fetched data
 for table_name, df in dataframes_dict.items():
     print(f"Data for table '{table_name}':")
     print(df.head(), "\n")
